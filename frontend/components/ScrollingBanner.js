e59e9bcff6d301f1c8826e8412cd85dbaf0d0fd0
Anna <annabaumann@laposte.net>
2019-01-03 23:13:19 +0100

    change scrolling banner

--- frontend/components/ScrollingBanner.js
+++ frontend/components/ScrollingBanner.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 
 class ScrollingBanner extends Component {
   constructor() {
@@ -9,19 +9,40 @@ class ScrollingBanner extends Component {
     const { text, moreClass } = this.props;
     return(
         <div className={`scrollingBanner ${moreClass}`}>
-            <div className={`scrollingBanner__text`}>
-                <span>{text}</span>
-            </div>
-            <div className={`scrollingBanner__text`}>
-                <span>{text}</span>
-            </div>
+            { moreClass === "scrollingBanner--vietnam" ?
+                <Fragment>
+                    <div className={`scrollingBanner__text`}>
+                        <span className={`scrollingBanner__span`}>{text}</span>
+                        <span  className={`scrollingBanner__span`}>{text}</span>
+                    </div> 
+                    <div className={`scrollingBanner__text`}>
+                        <span  className={`scrollingBanner__span`}>{text}</span>
+                        <span  className={`scrollingBanner__span`}> {text}</span>
+                    </div> 
+                    <div className={`scrollingBanner__text`}>
+                        <span  className={`scrollingBanner__span`}>{text}</span>
+                        <span  className={`scrollingBanner__span`}>{text}</span>
+                    </div> 
+                </Fragment>
+
+                :
+                <Fragment>
+                    <div className={`scrollingBanner__text`}>
+                        <span>{text}</span>
+                    </div>
+                    <div className={`scrollingBanner__text`}>
+                        <span>{text}</span>
+                    </div>
+                </Fragment>
+            }
             { moreClass === "scrollingBanner--about" ?
                 <div className={`scrollingBanner__text`}>
                     <span>{text}</span>
                 </div> 
                 :
                 ""
-             }
+            }
+
             
         </div>
     )
